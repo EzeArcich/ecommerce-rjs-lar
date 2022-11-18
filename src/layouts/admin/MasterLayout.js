9c4ba7fd1bcac6f498d9451bfd76b3a2c6b8c031
EzeArcich <97700576+EzeArcich@users.noreply.github.com>
2022-11-18 16:48:43 -0300

    first commit

---
+++ src/layouts/admin/MasterLayout.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const MasterLayout = () => {
+    return (
+        <div className="sb-nav-fixed">
+            <Navbar />
+            <div id="layoutSidenav">
+                <div id="layoutSidenav_nav">
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default MasterLayout;
